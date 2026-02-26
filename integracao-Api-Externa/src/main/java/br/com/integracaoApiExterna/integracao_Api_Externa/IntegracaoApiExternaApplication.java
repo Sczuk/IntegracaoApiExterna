baf132ead6db14fc5849108b39dba9be8880b28b
Sczuk <hgreasczuk@gmail.com>
2026-02-26 16:13:59 -0300

    chore(docker): config DockerFile

---
+++ integracao-Api-Externa/src/main/java/br/com/integracaoApiExterna/integracao_Api_Externa/IntegracaoApiExternaApplication.java
@@ -0,0 +1,13 @@
+package br.com.integracaoApiExterna.integracao_Api_Externa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracaoApiExternaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracaoApiExternaApplication.class, args);
+	}
+
+}
---
+++ integracao-Api-Externa/src/test/java/br/com/integracaoApiExterna/integracao_Api_Externa/IntegracaoApiExternaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.integracaoApiExterna.integracao_Api_Externa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegracaoApiExternaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
